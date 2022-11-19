f633ad632512a4505952d5420fc022991eb3c205
TrinidadMartiG <trinidad.martig@gmail.com>
2022-11-19 13:37:35 +0000

    added tables and relationships

--- src/models.py
+++ src/models.py
@@ -8,27 +8,40 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     lastname = Column(String(250), nullable=False)
     username = Column(Integer, nullable=False, unique=False)
     email = Column(String(250), nullable=False, unique=True)
+    rel = relationship("posts")
 
 class Post(Base):
-    __tablename__ = 'post'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    __tablename__ = "posts"
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeingKey("person.id"))
-    rel = relationship("Person")
+    user_id = Column(Integer, ForeignKey("users.id"))
 
 
-    def to_dict(self):
-        return {}
+class SavedPost(Base):
+    __tablename__ = "savedposts"
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey("posts.id"))
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+class Comment(Base):
+    __tablename__ = "comments" 
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey("posts.id"))
+    user_id = Column(Integer, ForeignKey("users.id"))
+    bodytext = Column(String(500))
+
+ 
+class Follower(Base):
+    __tablename__ = "followers"
+    id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey("users.id"))
+    user_to_id = Column(Integer, ForeignKey("users.id"))   
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
